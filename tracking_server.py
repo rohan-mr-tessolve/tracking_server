4215db824194c3c76ca00f0a1097ab88c6613038
rohan-mr-tessolve <rohanraghavendra.morappanavar@tessolve.com>
2025-09-02 12:19:46 +0530

    Update tracking_server.py

--- tracking_server.py
+++ tracking_server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, redirect, jsonify
 import sqlite3
 from datetime import datetime
-from curses import COLS
+from flask_cors  import CORS
 app = Flask(__name__)
 CORS(app)
 @app.route('/track_click')
